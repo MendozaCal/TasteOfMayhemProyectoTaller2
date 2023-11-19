43488c013d7ecceed31a14f9947fa878e76f45e7
RicardoPARIONA <hepsoricardo@gmail.com>
2023-11-18 19:41:42 -0500

    PMA-4 Movimiento Vegatales

---
+++ Assets/Scripts/MoveVegatables.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveVegatables : MonoBehaviour
+{
+    [SerializeField] private float Speed;
+    private Vector2 RandomPosition;
+    private float Distance = 2f;
+    private Vector2 InicialPoint;
+    private float timer = 0;
+    [SerializeField] public Transform player;
+    [SerializeField] public bool Follow = true;
+    [SerializeField] public bool MoveRandom = true;
+    private void Start()
+    {
+        InicialPoint = transform.position;
+        Direccion();
+    }
+    private void Direccion()
+    {
+        float X = Random.Range(-5, 5);
+        float Y = Random.Range(-5, 5);
+        Vector2 RandomValue = new Vector2(X, Y);
+        RandomPosition = InicialPoint + RandomValue * Distance;
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (player.GetComponent<MovePlayer>().canMove == false)
+        {
+            Follow = false;
+            if (Follow != true && MoveRandom == true)
+            {
+                StartCoroutine(VegetablesMove());
+                Movee();
+            }
+        }
+
+        else
+        {
+            if (MoveRandom == true)
+            {
+                Movee();
+            }
+            else
+            {
+                StartCoroutine(VegetablesMove());
+            }
+        }
+    }
+    private void Movee()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, RandomPosition, Speed * Time.deltaTime);
+        if (timer > 2)
+        {
+            if (Vector2.Distance(transform.position, InicialPoint) > 0.1f)
+            {
+                timer = 0;
+                Direccion();
+            }
+            if (Vector2.Distance(transform.position, RandomPosition) < 0.1f)
+            {
+                timer = 0;
+                Direccion();
+            }
+        }
+    }
+    IEnumerator VegetablesMove()
+    {
+        yield return new WaitForSeconds(5);
+        Follow = true;
+        MoveRandom = true;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("cucharon"))
+        {
+            Follow = false;
+            MoveRandom = false;
+        }
+    }
+}
+

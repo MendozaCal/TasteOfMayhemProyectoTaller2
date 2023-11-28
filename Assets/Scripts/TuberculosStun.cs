479734b6460136e1eb27fb640d4bb191b4bd745a
Ricardo <alumnotlsch@campustls.local>
2023-11-28 15:38:33 -0500

    PMA_05 ATAQUE TUBERCULOS

--- Assets/Scripts/TuberculosStun.cs
+++ Assets/Scripts/TuberculosStun.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class TuberculosStun : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
+    private GameObject player;
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))

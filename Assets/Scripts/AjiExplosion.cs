8fc9773a8915c40379e56f29242ecaa4ee569d57
Ricardo <alumnotlsch@campustls.local>
2023-11-28 15:36:32 -0500

    PMA_20 Ataque aji

--- Assets/Scripts/AjiExplosion.cs
+++ Assets/Scripts/AjiExplosion.cs
@@ -1,30 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
-public class AjiExplosion : MonoBehaviour
+public class AjiExplosio : MonoBehaviour
 {
-    [SerializeField] private Transform player;
+    private GameObject player;
     [SerializeField] public float DistanciaMin;
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
     private void ExplosionFire()
     {
         Vector2 OrigenRaycast = transform.position;
-        Vector2 DireccionRycast = ((Vector2)player.position - OrigenRaycast).normalized;
+        Vector2 DireccionRycast = ((Vector2)player.transform.position - OrigenRaycast).normalized;
         RaycastHit2D HitImformation = Physics2D.Raycast(OrigenRaycast, DireccionRycast, DistanciaMin);
         if (HitImformation.collider != null)
         {
             player.GetComponent<ShootMouse>().enabled = false;
         }
     }
-    private void OnDestroy()
-    {
-        ExplosionFire();
-    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
+            ExplosionFire();
         }
     }
 }

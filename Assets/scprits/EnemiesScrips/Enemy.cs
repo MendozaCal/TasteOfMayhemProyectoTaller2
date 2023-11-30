98ecb1752c50ed81c0aae8a8e2fc61e6a7f3d85c
Emerson Alarcon <emersonalarcon112@gmail.com>
2023-11-30 14:01:22 -0300

    Enemigo/Mejorado

--- Assets/scprits/EnemiesScrips/Enemy.cs
+++ Assets/scprits/EnemiesScrips/Enemy.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public int damageValue = 1;
+
+    private Spawner spawner;
+
+    public float speed = 3f;
+    public float detectionRadius = 5f;
+    private Transform player;
+    private bool playerDetected = false;
     public float velocidad = 2.0f;
     private bool comportamientoNormal = true;
-    [SerializeField] private float duracionAturdimiento = 20f; 
+    [SerializeField] private float duracionAturdimiento = 20f;
     [SerializeField] private float tiempoAturdimiento = 0.0f;
     private Vector3 posicionInicial;
 
     private void Start()
     {
+        {
             posicionInicial = transform.position;
+        }
+        spawner = GameObject.FindWithTag("Spawner").GetComponent<Spawner>();
+
+        if (spawner == null)
+        {
+            Debug.LogError("No se encontró el script del spawner.");
+        }
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        if (player == null)
+        {
+            Debug.LogError("No se encontró el objeto del jugador con la etiqueta 'Player'");
+        }
     }
 
     void Update()
@@ -47,6 +70,21 @@ public class Enemy : MonoBehaviour
                 transform.position = posicionInicial;
 
             }
+            if (Vector2.Distance(transform.position, player.position) < detectionRadius)
+            {
+                playerDetected = true;
+            }
+            else
+            {
+                playerDetected = false;
+            }
+
+            if (playerDetected)
+            {
+                Vector3 direction = (player.position - transform.position).normalized;
+
+                transform.Translate(direction * speed * Time.deltaTime);
+            }
         }
     }
 
@@ -57,9 +95,19 @@ public class Enemy : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("cucharon")) 
+        if (collision.gameObject.CompareTag("cucharon"))
         {
             Aturdir();
         }
+        if (collision.CompareTag("Player"))
+        {
+
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        spawner.EnemyDied();
     }
 }
